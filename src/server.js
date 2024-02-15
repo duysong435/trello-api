87b80664d37b068fb5143b2335077e7c7663ab77
duysong435 <duysong435@gmail.com>
2024-02-15 13:59:59 +0700

    add router

--- src/server.js
+++ src/server.js
@@ -4,13 +4,12 @@ import express from 'express'
 import exitHook from 'async-exit-hook'
 import { CLOSE_DB, CONECT_DB } from './config/mongodb'
 import { env } from './config/environment'
+import { APIs_V1 } from '~/routes/v1'
 const START_SERVER = () => {
 
   const app = express()
 
-  app.get('/', async (req, res) => {
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`App running at ${env.APP_HOST}:${env.APP_PORT}`)
