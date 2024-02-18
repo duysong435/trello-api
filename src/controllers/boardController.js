ce31b530dacc5ccc3eff0b81fb6a5acb8334924c
duysong435 <duysong435@gmail.com>
2024-02-18 13:14:27 +0700

    add  error handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,24 +1,19 @@
 import { StatusCodes } from 'http-status-codes'
-
+import ApiError from '~/utils/ApiError'
 const createNew = async (req, res, next) => {
   try {
 
-    console.log(req.body)
-    console.log(req.query)
-    console.log(req.params)
-    console.log(req.files)
-    console.log(req.cookies)
-    console.log(req.jwtDecoded)
+    // console.log(req.body)
+    // console.log(req.query)
+    // console.log(req.params)
+    // console.log(req.files)
+    // console.log(req.cookies)
+    // console.log(req.jwtDecoded)
     //Điều hướng sang tầng service
-
     // Có kết quả thì trả về phía client
 
     res.status(StatusCodes.CREATED).json({ message: 'API v1 post board' })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+  } catch (error) { next(error) }
 }
 
 export const boardController = {
