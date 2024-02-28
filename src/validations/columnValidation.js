66decbeea12d3128acabc929d428cf16b71a7260
duysong435 <duysong435@gmail.com>
2024-02-28 09:23:55 +0700

    drag card in column current with api

--- src/validations/columnValidation.js
+++ src/validations/columnValidation.js
@@ -20,6 +20,34 @@ const createNew = async (req, res, next) => {
 
 }
 
+const update = async (req, res, next) => {
+  // Lưu ý không dùng hàm require() trong trường hợp update
+  const correctCondition = Joi.object({
+    //Nếu cần làm tính năng di chuyển Column sang board khác thì mới thêm validate BoardId
+    // boardId: Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+    title: Joi.string().min(3).max(50).trim().strict(),
+    cardOrderIds: Joi.array().items(
+      Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+    )
+  })
+  try {
+    await correctCondition.validateAsync(req.body, {
+      abortEarly: false,
+      //  Đối với trường hợp update, cho phép Unknow để không cần đẩy một sô field lên
+      allowUnknown: true
+    })
+    // Validate dữ liệu hợp lệ chuyển sang controller
+    next()
+  } catch (error) {
+    // const errMessage = new Error(error).message
+    // const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errMessage)
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
+
+  }
+
+}
+
 export const columnValidation = {
-  createNew
+  createNew,
+  update
 }
