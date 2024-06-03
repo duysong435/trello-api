74fc2ea5062743c0fd72db675a4f649ef885e884
Shine <meotoameo@gmail.com>
2024-06-03 08:42:47 +0700

    google login

--- src/routes/v1/userRouter.js
+++ src/routes/v1/userRouter.js
@@ -7,7 +7,7 @@ const Router = express.Router();
 Router.route("/signup").post(userValidation.signUp, userController.signUp);
 Router.route("/login").post(userValidation.login, userController.login);
 
-// Router.use(authentication);
+Router.use(authentication);
 
 Router.route("/handlerRefreshToken").post(userController.handlerRefreshToken);
 Router.route("/logout").post(userController.logout);
